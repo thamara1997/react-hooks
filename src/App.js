139cca0cced15ca0e065b52736e34b67af8d2f55
thamara1997 <darshanathamara143@gmail.com>
2024-12-04 06:37:30 +0530

    new route added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import UseState from "./components/UseState";
 import UseEffect from "./components/UseEffect";
 import { createContext } from "react";
@@ -15,16 +16,47 @@ import UseCallBack from "./components/UseCallBack";
 function App() {
   return (
     <LoginContextProvider>
-      <div>
-        {/* <UseState /> */}
-        {/* <UseEffect /> */}
-        {/* <UseContext /> */}
-        {/* <UseRef /> */}
-        {/* <UseReducer /> */}
-        {/* <UseLayoutEffect /> */}
-        {/* <UseMemo /> */}
-        <UseCallBack />
-      </div>
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/use-state">UseState</Link>
+            </li>
+            <li>
+              <Link to="/use-effect">UseEffect</Link>
+            </li>
+            <li>
+              <Link to="/use-context">UseContext</Link>
+            </li>
+            <li>
+              <Link to="/use-ref">UseRef</Link>
+            </li>
+            <li>
+              <Link to="/use-reducer">UseReducer</Link>
+            </li>
+            <li>
+              <Link to="/use-layout-effect">UseLayoutEffect</Link>
+            </li>
+            <li>
+              <Link to="/use-memo">UseMemo</Link>
+            </li>
+            <li>
+              <Link to="/use-callback">UseCallBack</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/use-state" element={<UseState />} />
+          <Route path="/use-effect" element={<UseEffect />} />
+          <Route path="/use-context" element={<UseContext />} />
+          <Route path="/use-ref" element={<UseRef />} />
+          <Route path="/use-reducer" element={<UseReducer />} />
+          <Route path="/use-layout-effect" element={<UseLayoutEffect />} />
+          <Route path="/use-memo" element={<UseMemo />} />
+          <Route path="/use-callback" element={<UseCallBack />} />
+        </Routes>
+      </Router>
     </LoginContextProvider>
   );
 }
